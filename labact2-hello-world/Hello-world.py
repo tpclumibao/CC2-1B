58e46cd7753bfa6672a057c57433529bcba3760a
Lumibao, Tom Peycee C <145944398+tpclumibao@users.noreply.github.com>
2023-09-27 17:51:13 +0800

    Add files via upload

---
+++ labact2-hello-world/Hello-world.py
@@ -0,0 +1,20 @@
+#OH O STINKY
+#Enter name
+name = input('What is your name? ')
+#Enter Age
+age_input = int(input('What is your age?'))
+#Value of Age to int
+age = int(age_input)
+#Value of Age Next Year
+age_next_year = age+1
+modulo =5%3
+#Value of pi
+pi = 3.1416
+
+print (f'Hello!, {name}',end=' & ')
+print ('Hello',name,sep='%%%%')
+print ('Hello '+name)
+print (f'My age next year is{(age_next_year)}')
+print (f'Pi value is {(pi)}')
+print (f'5 modulo 3 is {(5%3)}')
+
